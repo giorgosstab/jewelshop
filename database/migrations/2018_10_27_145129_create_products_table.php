1b6634111e0129c3e21dff34c980362cd61fe57e
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2018-10-27 19:33:10 +0300

    edit migration products

--- database/migrations/2018_10_27_145129_create_products_table.php
+++ database/migrations/2018_10_27_145129_create_products_table.php
@@ -17,7 +17,7 @@ class CreateProductsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('slug')->unique();
-            $table->string('details')->nullable();
+            $table->string('sku')->unique();
             $table->integer('price');
             $table->text('description');
             $table->boolean('featured')->default(false);
