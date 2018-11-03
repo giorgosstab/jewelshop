08adc4bab0f1e27cc4dc47d0a2fe95bef4792f05
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2018-11-03 22:19:53 +0200

    Fix sources dynamic

--- resources/views/shop/layouts/related-products.blade.php
+++ resources/views/shop/layouts/related-products.blade.php
@@ -1,63 +1,63 @@
-<div class="col-md-6 col-sm-12 col-xl-12 wow fadeIn">
-    <div class="cat-div">
-        <h2>Related Products</h2>
-        <div id="carousel-example-generic3" class="carousel slide" data-ride="carousel">
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <div class="product-scroll">
-                        <div class="row">
-                            <div class="col-md-6 col-sm-2 col-4"><img  src="assets/images/scroll-2.jpg" alt="" title="" class="img-fluid"></div>
-                            <div class="col-md-6 col-sm-9 col-8">
-                                <h3>Product name</h3>
-                                <div> <img  src="assets/images/str1.jpg" alt="" title=""><img  src="assets/images/str1.jpg" alt="" title=""> <img  src="assets/images/str1.jpg" alt="" title=""><img  src="assets/images/str1.jpg" alt="" title=""><img  src="assets/images/str2.jpg" alt="" title=""></div>
-                                <h4>$600.00</h4>
+<div class="cat-div">
+    <h2>Related Products</h2>
+    <div id="carousel-example-generic2" class="carousel slide" data-ride="carousel">
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                @foreach($mightAlsoLike->slice(0, 2) as $product)
+                    <a href="{{ route('shop.products.details', $product->slug) }}">
+                        <div class="product-scroll">
+                            <div class="row AlsoLike-border">
+                                <div class="col-md-6 col-sm-2 col-xs-6"><img  src="{{ asset('assets/images/scroll-2.jpg') }}" alt="" title="" class="img-fluid"></div>
+                                <div class="col-md-6 col-sm-9 col-xs-6">
+                                    <h3>{{ $product->name }}</h3>
+                                    <div>
+                                        <img  src="{{ asset('assets/images/str1.jpg') }}" alt="" title="">
+                                        <img  src="{{ asset('assets/images/str1.jpg') }}" alt="" title="">
+                                        <img  src="{{ asset('assets/images/str1.jpg') }}" alt="" title="">
+                                        <img  src="{{ asset('assets/images/str1.jpg') }}" alt="" title="">
+                                        <img  src="{{ asset('assets/images/str2.jpg') }}" alt="" title="">
+                                    </div>
+                                    <h4>€{{ $product->presentPrice() }}</h4>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </a>
                     <div class="clearfix"></div>
-                    <div class="product-scroll">
-                        <div class="row">
-                            <div class="col-md-6 col-sm-2 col-4"><img  src="assets/images/scroll-2.jpg" class="img-fluid" alt="" title=""></div>
-                            <div class="col-md-6 col-sm-9 col-8">
-                                <h3>Product name</h3>
-                                <div> <img  src="assets/images/str1.jpg" alt="" title=""><img  src="assets/images/str1.jpg" alt="" title=""> <img  src="assets/images/str1.jpg" alt="" title=""><img  src="assets/images/str1.jpg" alt="" title=""><img  src="assets/images/str2.jpg" alt="" title=""></div>
-                                <h4>$600.00</h4>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="product-scroll">
-                        <div class="row">
-                            <div class="col-md-6 col-sm-2 col-4"><img  src="assets/images/scroll-2.jpg" class="img-fluid" alt="" title=""></div>
-                            <div class="col-md-6 col-sm-9 col-8">
-                                <h3>Product name</h3>
-                                <div> <img  src="assets/images/str1.jpg" alt="" title=""><img  src="assets/images/str1.jpg" alt="" title=""> <img  src="assets/images/str1.jpg" alt="" title=""><img  src="assets/images/str1.jpg" alt="" title=""><img  src="assets/images/str2.jpg" alt="" title=""></div>
-                                <h4>$600.00</h4>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="clearfix"></div>
-                    <div class="product-scroll">
-                        <div class="row">
-                            <div class="col-md-6 col-sm-2 col-4"><img  src="assets/images/scroll-2.jpg" alt="" title="" class="img-fluid"></div>
-                            <div class="col-md-6 col-sm-9 col-8">
-                                <h3>Product name</h3>
-                                <div><img  src="assets/images/str1.jpg" alt="" title=""><img  src="assets/images/str1.jpg" alt="" title=""><img  src="assets/images/str1.jpg" alt="" title=""><img  src="assets/images/str1.jpg" alt="" title=""><img  src="assets/images/str2.jpg" alt="" title=""></div>
-                                <h4>$600.00</h4>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
+                <div class="clearfix"></div>
             </div>
-            <!-- Controls -->
-            <a class="left arrow-left" href="#carousel-example-generic3" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left"></span>
-            </a>
-            <a class="right arrow-right" href="#carousel-example-generic3" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right"></span>
-            </a>
+            <div class="carousel-item">
+                @foreach($mightAlsoLike->slice(2, 4) as $product)
+                    <a href="{{ route('shop.products.details', $product->slug) }}">
+                        <div class="product-scroll">
+                            <div class="row AlsoLike-border">
+                                <div class="col-md-6 col-sm-2 col-xs-6"><img  src="{{ asset('assets/images/scroll-2.jpg') }}" alt="" title="" class="img-fluid"></div>
+                                <div class="col-md-6 col-sm-9 col-xs-6">
+                                    <h3>{{ $product->name }}</h3>
+                                    <div>
+                                        <img  src="{{ asset('assets/images/str1.jpg') }}" alt="" title="">
+                                        <img  src="{{ asset('assets/images/str1.jpg') }}" alt="" title="">
+                                        <img  src="{{ asset('assets/images/str1.jpg') }}" alt="" title="">
+                                        <img  src="{{ asset('assets/images/str1.jpg') }}" alt="" title="">
+                                        <img  src="{{ asset('assets/images/str2.jpg') }}" alt="" title="">
+                                    </div>
+                                    <h4>€{{ $product->presentPrice() }}</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                    <div class="clearfix"></div>
+                @endforeach
+                <div class="clearfix"></div>
+            </div>
         </div>
+        <!-- Controls -->
+        <a class="left arrow-left" href="#carousel-example-generic2" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+        </a>
+        <a class="right arrow-right" href="#carousel-example-generic2" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+        </a>
     </div>
 </div>
