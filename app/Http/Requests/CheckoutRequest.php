7282f513b436438bc2c10cc9a6ae10638879b990
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2018-11-10 14:36:29 +0200

    Add backend validation form checkout

---
+++ app/Http/Requests/CheckoutRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CheckoutRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+
+//        $emailValidation = auth()->user() ? 'required|email' : 'required|email|unique:users';
+        return [
+            'fname' => 'required',
+            'lname' => 'required',
+            'email' => 'required|email',
+            'address' => 'required',
+            'city' => 'required',
+            'zip-code' => 'required',
+            'locality' => 'required',
+            'country' => 'required',
+            'phone' => 'required',
+        ];
+    }
+}
