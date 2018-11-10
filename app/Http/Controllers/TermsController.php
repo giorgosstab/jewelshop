8bec1de47109767a2ea1db428a76c5cd5fbc9c52
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2018-11-10 06:22:36 +0200

    Add Terms & Conditions Page

---
+++ app/Http/Controllers/TermsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class TermsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('shop.privacy.main');
+    }
+}
---
+++ resources/views/shop/privacy/main.blade.php
@@ -0,0 +1,89 @@
+@extends('shop.main')
+
+@section('title', '| Terms & Conditions')
+
+@section('extra-css')
+
+@endsection
+
+@section('content')
+    <!--page heading-->
+    <section>
+        <div class="inner-bg">
+            <div class="inner-head wow fadeInDown">
+                <h3 >Terms & Conditions</h3>
+            </div>
+        </div>
+    </section>
+    <!--container-->
+    <div class="container">
+        <div class="shop-in">
+            <div>
+                <div class="bread2">
+                    <ul>
+                        <li><a href="{{ route('shop.home.index') }}">HOME</a>
+                        <li>/</li>
+                        <li>Terms & Conditions</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="clearfix"> </div>
+            <div>
+                <div class="text-style2"><p class="wow fadeIn">The following terms and conditions Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p></div>
+                <div class="inner-section terms-text">
+                    <div class="wow fadeIn">
+                        <h2>1. Acceptance</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. </p>
+                    </div>
+                    <div class="wow fadeIn">
+                        <h2>2. Charges</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. </p>
+                    </div>
+                    <div class="wow fadeIn">
+                        <h2>3. Client Review</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. </p>
+                    </div>
+                    <div class="wow fadeIn">
+                        <h2>4. Turnaround Time and Content Control</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. </p>
+                    </div>
+                    <div class="wow fadeIn">
+                        <h2>6. Payment</h2>
+                        <p>Invoices will be provided by Wombat Creative Limited upon completion but before publishing the live website. Invoices are normally sent via email; however, the Client may choose to receive hard copy invoices. Invoices are due upon receipt. Accounts that remain unpaid thirty (30) days after the date of the invoice will be assessed a service charge in the amount of the higher of one and one-half percent (1.5%) or £30 per month of the total amount due.</p>
+                    </div>
+                    <div class="wow fadeIn">
+                        <h2>7. Additional Expenses</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. </p>
+                    </div>
+                    <div class="wow fadeIn">
+                        <h2>8. Web Browsers</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. </p>
+                    </div>
+                    <div class="wow fadeIn">
+                        <h2>9. Default</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. </p>
+                    </div>
+                    <div class="wow fadeIn">
+                        <h2>10. Termination</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. </p>
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+    <!--container-->
+@endsection
+
+@section('extra-script')
+
+@endsection
