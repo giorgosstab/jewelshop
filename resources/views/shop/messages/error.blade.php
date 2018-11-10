7adda3326066c4a25e2b789b2c58734044f0b074
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2018-11-10 15:23:03 +0200

    Fix error message

--- resources/views/shop/messages/error.blade.php
+++ resources/views/shop/messages/error.blade.php
@@ -1,7 +1,11 @@
 @if(count($errors) > 0)
-    <div class="alert alert-danger">
-        @foreach($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
+    <div class="notice notice-danger" id="message">
+        <button type="button" class="close" onclick="this.parentElement.style.display='none';" ><span aria-hidden="true">&times</span></button>
+        <strong>Danger!</strong>
+        <em>
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </em>
     </div>
 @endif
