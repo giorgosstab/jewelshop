83b3f3ec464ec1a0c690861b119131b6c7423642
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2018-11-10 07:21:57 +0200

    Add confirmation controller index

---
+++ app/Http/Controllers/ConfirmationController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class ConfirmationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        if(!session()->has('success_message')){
+            redirect('/');
+        }
+        return view('shop.checkout.confirm');
+    }
+}
