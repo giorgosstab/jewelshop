fd6e184afe3ab7d30829f7f466e6b8e4f74b6311
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2018-11-12 18:19:36 +0200

    Change Names

--- app/Http/Requests/CheckoutRequest.php
+++ app/Http/Requests/CheckoutRequest.php
@@ -26,15 +26,15 @@ class CheckoutRequest extends FormRequest
 
 //        $emailValidation = auth()->user() ? 'required|email' : 'required|email|unique:users';
         return [
-            'fname' => 'required',
-            'lname' => 'required',
-            'email' => 'required|email',
-            'address' => 'required',
-            'city' => 'required',
-            'zip-code' => 'required',
+            'First Name' => 'required',
+            'Last Name' => 'required',
+            'E-mail' => 'required|email',
+            'Address' => 'required',
+            'City' => 'required',
+            'Postal Code' => 'required',
             'locality' => 'required',
-            'country' => 'required',
-            'phone' => 'required',
+            'Country' => 'required',
+            'Phone Number' => 'required',
         ];
     }
 }
