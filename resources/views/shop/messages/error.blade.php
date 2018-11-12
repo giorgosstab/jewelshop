76837074ca5c69e595b5dd757f22f4e4292dc7b9
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2018-11-12 18:19:06 +0200

    Remove id for fade message

--- resources/views/shop/messages/error.blade.php
+++ resources/views/shop/messages/error.blade.php
@@ -1,5 +1,5 @@
 @if(count($errors) > 0)
-    <div class="notice notice-danger" id="message">
+    <div class="notice notice-danger">
         <button type="button" class="close" onclick="this.parentElement.style.display='none';" ><span aria-hidden="true">&times</span></button>
         <strong>Danger!</strong>
         <em>
