6fefab8a087fb3472b618cb0f18704ced98cfda8
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2018-11-14 15:14:56 +0200

    Change align left

--- resources/views/shop/messages/error.blade.php
+++ resources/views/shop/messages/error.blade.php
@@ -1,5 +1,5 @@
 @if(count($errors) > 0)
-    <div class="notice notice-warning">
+    <div class="notice notice-warning notice-warning-left">
         <button type="button" class="close" onclick="this.parentElement.style.display='none';" ><span aria-hidden="true">&times</span></button>
         <strong>Warning!</strong>
         <em>
