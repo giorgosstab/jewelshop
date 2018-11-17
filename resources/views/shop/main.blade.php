c655a37c9cd9e3460c7181af96e1bdbb3ed57382
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2018-11-17 13:18:09 +0200

    Add csrf token

--- resources/views/shop/main.blade.php
+++ resources/views/shop/main.blade.php
@@ -2,6 +2,10 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="Jewellery Shoppe">
         <meta name="author" content="Jewellery Shoppe">
