10957be859252298991991db15f5d8ce2daa052d
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2018-11-18 17:30:36 +0200

    Add category_product table NxN and fix relation attach at seed

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -19,7 +19,7 @@ class ProductsTableSeeder extends Seeder
                 'sku' => rand(00000001, 99999999).$i,
                 'price' => rand(300, 1099),
                 'description' => 'Lorem ipsum ' . $i . 'dolor sit amet, consectetur adipiscing elit. Cras ac nisi eu tortor sodales ornare vitae non dolor. Praesent elit velit, blandit vitae nibh eget, feugiat tincidunt diam. Nam tincidunt auctor tellus, nec dictum risus ullamcorper quis.'
-            ]); //->categoriesJewels()->attach(1)
+            ])->categoriesJewels()->attach(1);
         }
 
         for($i = 1; $i <= 15; $i++){
@@ -29,7 +29,7 @@ class ProductsTableSeeder extends Seeder
                 'sku' => rand(00000001, 99999999).$i.$i,
                 'price' => rand(300, 1099),
                 'description' => 'Lorem ipsum ' . $i . 'dolor sit amet, consectetur adipiscing elit. Cras ac nisi eu tortor sodales ornare vitae non dolor. Praesent elit velit, blandit vitae nibh eget, feugiat tincidunt diam. Nam tincidunt auctor tellus, nec dictum risus ullamcorper quis.'
-            ]);
+            ])->categoriesJewels()->attach(2);
         }
 
     }
