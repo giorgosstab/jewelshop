ffdb7fe46172e875216fedb4e8cdf2a4b99bd63d
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2018-11-22 15:50:05 +0200

    Fix category key and name of product

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -19,17 +19,17 @@ class ProductsTableSeeder extends Seeder
                 'sku' => rand(00000001, 99999999).$i,
                 'price' => rand(300, 1099),
                 'description' => 'Lorem ipsum ' . $i . 'dolor sit amet, consectetur adipiscing elit. Cras ac nisi eu tortor sodales ornare vitae non dolor. Praesent elit velit, blandit vitae nibh eget, feugiat tincidunt diam. Nam tincidunt auctor tellus, nec dictum risus ullamcorper quis.'
-            ])->categoriesJewels()->attach(1);
+            ])->categoriesJewels()->attach(11);
         }
 
         for($i = 1; $i <= 15; $i++){
             Product::create([
-                'name' => 'Ring Product ' .$i,
-                'slug' => 'ring-' .$i,
+                'name' => 'Diamond Ring Product ' .$i,
+                'slug' => 'diamond-ring-' .$i,
                 'sku' => rand(00000001, 99999999).$i.$i,
                 'price' => rand(300, 1099),
                 'description' => 'Lorem ipsum ' . $i . 'dolor sit amet, consectetur adipiscing elit. Cras ac nisi eu tortor sodales ornare vitae non dolor. Praesent elit velit, blandit vitae nibh eget, feugiat tincidunt diam. Nam tincidunt auctor tellus, nec dictum risus ullamcorper quis.'
-            ])->categoriesJewels()->attach(2);
+            ])->categoriesJewels()->attach(13);
         }
 
     }
