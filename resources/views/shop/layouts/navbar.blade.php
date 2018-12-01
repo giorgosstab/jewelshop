ea9de0878169271ba58ab6d6a32ecca007df4d30
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2018-12-01 22:35:56 +0200

    Add menus voyager dynamically

--- resources/views/shop/layouts/navbar.blade.php
+++ resources/views/shop/layouts/navbar.blade.php
@@ -104,58 +104,7 @@
         <div class="clearfix"></div>
         <!-- Navbar links -->
         <div class="collapse navbar-collapse" id="collapsibleNavbar">
-            <ul class="navbar-nav">
-                <li class="nav-item"><a  class="nav-link"  href="{{ route('shop.home.index') }}">Home</a></li>
-                <li class="nav-item"> <a class="nav-link " href="about-us.html">About</a> </li>
-                <li class="nav-item dropdown  mega-dropdown"> <a class="nav-link dropdown-toggle text-uppercase no-caret" id="navbarDropdownMenuLink1" href="{{ route('shop.products.index') }}" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false"> Shop </a>
-                    <div class="dropdown-menu mega-menu v-2 row m-0 z-depth-1 special-color" aria-labelledby="navbarDropdownMenuLink1">
-                        <div class="row mx-md-4 mx-1">
-                            <div class="col-md-2 col-lg-4 col-xl-4 sub-menu my-xl-5 mt-md-5 mt-1 mb-1">
-                                <h6 class="sub-title text-uppercase font-weight-bold white-text">Related</h6>
-                                <!--Featured image-->
-                                <div class="view overlay mb-3 z-depth-1"> <img src="{{ asset('assets/images/menu-img.jpg') }}" class="img-fluid" alt="First sample image">
-                                    <div class="mask rgba-white-slight flex-center">
-                                        <p></p>
-                                    </div>
-                                </div>
-                                <h4 class="mb-2"><a class="news-title-2 pl-0" href="#">THE SAADIA DROP EARRINGS</a></h4>
-                                <p class="font-small text-uppercase white-text"><i class="fa fa-clock-o pr-2" aria-hidden="true"></i>July 17, 2017 / <i class="fa fa-comments-o px-1" aria-hidden="true"></i> 20</p>
-                            </div>
-                            <div class="col-md-10 col-lg-8 col-xl-8">
-                                <div class="row">
-                                    @foreach($allSubCategories as $subCate)
-                                        <div class="col-sm-4 col-md-4 col-lg-3 col-xl-2 sub-menu my-xl-5 mt-md-5 mt-1 mb-1">
-                                            <a class="menu-item" href="{{ route('shop.products.index', ['cat' => $subCate->slug]) }}"><h6 class="sub-title text-uppercase font-weight-bold white-text">{{ $subCate->name }}</h6></a>
-                                            <ul class="caret-style pl-0">
-                                                @foreach($subCate->subCategory as $firstNestedSub)
-                                                    <li class=""><a class="menu-item" href="{{ route('shop.products.index', ['sub' => $firstNestedSub->slug]) }}">{{ $firstNestedSub->name }}</a></li>
-                                                    @foreach($firstNestedSub->subCategory as $secondNestedSub)
-                                                        SecondNested : {{ $secondNestedSub->name }}<br>
-                                                        @foreach($secondNestedSub->subCategory as $thirdNestedSub)
-                                                            $thirdNested: {{ $thirdNestedSub->name }}
-                                                        @endforeach()
-                                                    @endforeach()
-                                                @endforeach()
-                                            </ul>
-                                        </div>
-                                    @endforeach()
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li class="nav-item"> <a class="nav-link" href="#">Blog</a> </li>
-                <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown"> Pages </a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item"  href="404.html">404</a>
-                        <a class="dropdown-item"  href="faq.html">FAQ</a>
-                        <a class="dropdown-item" href="privacy.html">Privacy</a>
-                        <a class="dropdown-item" href="coming-soon.html">Coming soon</a>
-                        <a class="dropdown-item" href="terms-and-conditions.html">Terms &amp; Condition</a>
-                    </div>
-                </li>
-                <li class="nav-item"><a  class="nav-link"  href="contact-us.html">Contact us</a></li>
-            </ul>
+            {{ menu('main', 'shop.partials.menus.main') }}
         </div>
     </div>
 </nav>
