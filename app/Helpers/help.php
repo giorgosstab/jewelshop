a55efda3de21e9338e3014639a11b68718da20ed
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2018-12-02 22:39:50 +0200

    Add product image path if exist

--- app/Helpers/help.php
+++ app/Helpers/help.php
@@ -6,5 +6,10 @@
  * Time: 6:03 AM
  */
 function presentPrice($price) {
-    return number_format(price, 2);
+    return number_format($price, 2);
+}
+
+function productImage($path)
+{
+    return file_exists('storage/'.$path) ? secure_asset('storage/'.$path) : secure_asset('storage/products/no_image.jpg');
 }
