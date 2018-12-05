5c1c9be3b266fe4d9da5b2e21f50bb9172262b7a
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2018-12-05 20:44:24 +0200

    Add about page

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class AboutController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('shop.about.main');
+    }
+}
