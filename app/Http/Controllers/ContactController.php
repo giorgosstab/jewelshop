c3c790061ad899fe7b4c895133f2c4b4433f5d9e
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2018-12-06 04:05:47 +0200

    Edit routes v2.0.0

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('shop.contact.main');
+    }
+}
