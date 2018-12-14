e6bfd7fa6d6ef5b76060fe01b1efd6ed97914bea
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2018-12-14 04:50:36 +0200

    Fix template for verify email account

--- resources/views/auth/passwords/verify.blade.php
+++ resources/views/auth/passwords/verify.blade.php
@@ -4,8 +4,21 @@
 Thank you for signing up for {{ config('app.name') }}. We're happy you're here!<br>
 Please click the button below to activate your account and start buying form us!
 
-@component('mail::button', ['url' => ''])
+@component('mail::button', ['url' => route('auth.activate',[
+                                'token' => $user->activation_token,
+                                'email' => $user->email
+                            ])
+            ])
     Activate
+@endcomponent
+
+@component('mail::subcopy')
+    If you’re having trouble clicking the "Activate" button, copy and paste the URL below
+    into your web browser: {!! route('auth.activate',[
+                                                        'token' => $user->activation_token,
+                                                        'email' => $user->email
+                                                    ])
+                            !!}
 @endcomponent
 
 Regards,<br>
