ab86de38aa2693eec8e5e635e8ace91cbd7cb0bf
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2018-12-14 03:21:53 +0200

    Add verify Route

--- routes/web.php
+++ routes/web.php
@@ -71,4 +71,6 @@ Route::group(['prefix' => 'admin'], function () {
 
 Auth::routes();
 
+Route::get('auth/activate', 'Auth\ActivationController@activate')->name('auth.activate');
+
 //Route::get('/home', 'HomeController@index')->name('home');
