19f0e804610622a9ecd9e0d7ea64c9ad815b70d5
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2018-12-19 22:24:09 +0200

    Change view login-register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,65 +25,105 @@
         </div>
     </section>
     <!--page heading-->
-    <!--container-->
-    <div class="container">
-        <div class="shop-in pl-0 pr-0">
-            <div>
-                <div class="bread2">
-                    <ul>
-                        <li><a href="{{ route('shop.home.index') }}">HOME</a>
-                        <li>/</li>
-                        <li>Sign In</li>
-                    </ul>
+    <div class="blog-bg">
+        <!--container-->
+        <div class="container">
+            <div class="shop-in pl-0 pr-0">
+                <div>
+                    <div class="bread2">
+                        <ul>
+                            <li><a href="{{ route('shop.home.index') }}">HOME</a>
+                            <li>/</li>
+                            <li>Sign In</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="clearfix"> </div>
+                @include('shop.messages.error')
+                <div class="row">
+                    <!--login-left-side-->
+                    <div class="col-md-8">
+                        <div class="blog-in">
+                        {!! Form::open(array('route'=>'login','method' => 'POST','class' => 'form-signin')) !!}
+                            {{ csrf_field() }}
+                            <div class="text-center mb-4">
+                                <h3 class="h2 mb-3 font-weight-normal">{{ __('Sign In User') }}</h3>
+                            </div>
+
+                            <div class="form-label-group">
+                                {{ Form::email('email', old('email'), array('class' => 'form-control', 'placeholder' => 'Email address', 'id' => 'inputEmail', $errors->has('email') ? ' is-invalid' : '','required', 'autofocus')) }}
+                                {{ Form::label('inputEmail', 'E-Mail Address') }}
+                            </div>
+
+                            <div class="form-label-group">
+                                <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" {{ $errors->has('password') ? ' is-invalid' : '' }} required>
+                                {{ Form::label('inputPassword', 'Password') }}
+                            </div>
+
+                            <div class="checkbox mb-3">
+                                <label class="form-check-label" for="remember">
+
+                                    {{--<input type="checkbox" value="remember-me">--}}
+                                    {{ Form::checkbox('remember', null, array('id' => 'inputPassword', old('remember') ? 'checked' : '')) }}
+                                    {{ __('Remember Me') }}
+                                </label>
+                            </div>
+                            {!! Form::button('Sign in',array('class'=>'btn btn-default btn-lg button-1 btn-block', 'type' => 'submit')) !!}
+                            <div class="clearfix"> </div><br>
+                            <div style="font-size: 12px;">
+                                <b>If you did not receive email verification please renew <a href="{{ route('auth.activate.showResendForm') }}"><span style="color: #dfb859">here</span></a></b>
+                            </div>
+                            <div class="form-group">
+                                <div class="boder3"></div>
+                                <p>
+                                    <center><a href="{{ route('password.request') }}">Forgot Password?</a><center>
+                                </p>
+                                <div class="boder3"></div>
+                            </div>
+                            <p class="mt-5 mb-3 text-muted text-center">© 2017 - {{ date('Y') }}</p>
+                            Email us if you have login problems :<a href="mailto:{{ setting('site.email') }}"> {{ setting('site.email') }} </a>
+                        {{ Form::close() }}
+                        </div>
+                        <div class="clearfix"> </div>
+                    </div>
+                    <!--login-right-side-->
+                    <div class="col-md-4 col-12">
+                        <div class="blog-in">
+                            <div class="mb-4">
+                                <h3 class="h2 mb-3 font-weight-normal">As Guest</h3>
+                            </div>
+                            <div class="clearfix"> </div>
+                            <div class="mb-4">
+                                <h3 class="h5 mb-3 font-weight-normal"><b>Save time for now.</b></h3>
+                                <h3 class="h5 mb-3 font-weight-normal">You dont need account to check out.Buy us guest!</h3>
+                            </div>
+                            <div class="clearfix"> </div>
+                            <div class="mb-4 secure">
+                                <a href="{{ route('shop.checkout.index') }}">
+                                    Continue as Guest <i class="fa fa-long-arrow-right"></i>
+                                </a>
+                            </div>
+                            <div class="clearfix"> </div><br>
+                            <div class="clearfix"> </div><br>
+                            <div class="mb-4">
+                                <h3 class="h2 mb-3 font-weight-normal">New Customer</h3>
+                            </div>
+                            <div class="mb-4">
+                                <h3 class="h5 mb-3 font-weight-normal"><b>Save time for later.</b></h3>
+                                <h3 class="h5 mb-3 font-weight-normal">Create an account for fast checkout and easy access to order history!</h3>
+                            </div>
+                            <div class="clearfix"> </div>
+                            <div class="mb-4 secure">
+                                <a href="{{ route('register') }}">
+                                    Create Account <i class="fa fa-long-arrow-right"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="clearfix"> </div>
-            @include('shop.messages.error')
-            <div class="clearfix"> </div>
-            {!! Form::open(array('route'=>'login','method' => 'POST','class' => 'form-signin')) !!}
-                {{ csrf_field() }}
-                <div class="text-center mb-4">
-                    <h3 class="h3 mb-3 font-weight-normal">{{ __('Sign In User') }}</h3>
-                </div>
-
-                <div class="form-label-group">
-                    {{ Form::email('email', old('email'), array('class' => 'form-control', 'placeholder' => 'Email address', 'id' => 'inputEmail', $errors->has('email') ? ' is-invalid' : '','required', 'autofocus')) }}
-                    {{ Form::label('inputEmail', 'E-Mail Address') }}
-                </div>
-
-                <div class="form-label-group">
-                    <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" {{ $errors->has('password') ? ' is-invalid' : '' }} required>
-                    {{ Form::label('inputPassword', 'Password') }}
-                </div>
-
-                <div class="checkbox mb-3">
-                    <label class="form-check-label" for="remember">
-
-                        {{--<input type="checkbox" value="remember-me">--}}
-                        {{ Form::checkbox('remember', null, array('id' => 'inputPassword', old('remember') ? 'checked' : '')) }}
-                        {{ __('Remember Me') }}
-                    </label>
-                </div>
-                {!! Form::button('Sign in',array('class'=>'btn btn-default btn-lg button-1 btn-block', 'type' => 'submit')) !!}
-                <div class="clearfix"> </div><br>
-                <div style="font-size: 12px;">
-                    <b>If you did not receive email verification please renew <a href="{{ route('auth.activate.showResendForm') }}"><span style="color: #dfb859">here</span></a></b>
-                </div>
-                <div class="form-group">
-                    <div class="boder3"></div>
-                    <p>
-                        <a href="{{ route('register') }}" style="float:left">Register New Account?</a>
-                        <a href="{{ route('password.request') }}" style="float:right">Forgot Password?</a>
-                    </p>
-                    <div class="boder3"></div>
-                </div>
-                <div class="clearfix"> </div><br>
-                <p class="mt-5 mb-3 text-muted text-center">© 2017 - {{ date('Y') }}</p>
-                Email us if you have login problems :<a href="mailto:{{ setting('site.email') }}"> {{ setting('site.email') }} </a>
-            {{ Form::close() }}
-            <div class="clearfix"> </div>
+            <div class="clearfix"></div>
         </div>
-        <div class="clearfix"></div>
     </div>
     <!--container-->
     <div class="clearfix"></div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,68 +26,111 @@
         </div>
     </section>
     <!--page heading-->
-    <!--container-->
-    <div class="container">
-        <div class="shop-in pl-0 pr-0">
-            <div>
-                <div class="bread2">
-                    <ul>
-                        <li><a href="{{ route('shop.home.index') }}">HOME</a>
-                        <li>/</li>
-                        <li>Sign Up</li>
-                    </ul>
+    <div class="blog-bg">
+        <!--container-->
+        <div class="container">
+            <div class="shop-in pl-0 pr-0">
+                <div>
+                    <div class="bread2">
+                        <ul>
+                            <li><a href="{{ route('shop.home.index') }}">HOME</a>
+                            <li>/</li>
+                            <li>Sign Up</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="clearfix"> </div>
+                @include('shop.messages.error')
+                <div class="row">
+                    <!--login-register-side-->
+                    <div class="col-md-8">
+                        <div class="blog-in">
+                            {!! Form::open(array('route'=>'register','method' => 'POST','class' => 'form-signup')) !!}
+                                {{ csrf_field() }}
+                                {!!  GoogleReCaptchaV3::render('register') !!}
+                                <div class="text-center mb-4">
+                                    <h3 class="h3 mb-3 font-weight-normal">{{ __('Sign Un New Profile') }}</h3>
+                                </div>
+
+                                <div class="form-label-group">
+                                    {{ Form::text('name', old('name'), array('class' => 'form-control', 'placeholder' => 'Full Name', 'id' => 'inputName', $errors->has('name') ? ' is-invalid' : '','required', 'autofocus')) }}
+                                    {{ Form::label('inputName', 'Full Name') }}
+                                </div>
+
+                                <div class="form-label-group">
+                                    {{ Form::email('email', old('email'), array('class' => 'form-control', 'placeholder' => 'Email address', 'id' => 'inputEmail', $errors->has('email') ? ' is-invalid' : '','required')) }}
+                                    {{ Form::label('inputEmail', 'E-Mail Address') }}
+                                </div>
+
+                                <div class="form-label-group">
+                                    <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" {{ $errors->has('password') ? ' is-invalid' : '' }} required>
+                                    {{ Form::label('inputPassword', 'Password') }}
+                                </div>
+
+                                <div class="form-label-group">
+                                    <input type="password" name="password_confirmation" id="inputPasswordConfirmed" class="form-control" placeholder="Re-type Password" required>
+                                    {{ Form::label('inputPasswordConfirmed', 'Re-type Password') }}
+                                </div>
+
+                            {!! Form::button('Create Acount',array('class'=>'btn btn-default btn-lg button-1 btn-block', 'type' => 'submit')) !!}
+                            <div class="clearfix"> </div><br>
+                            <div class="form-group">
+                                <div class="boder3"></div>
+                                <p>
+                                    <div style="float:left">
+                                        Already have an account? <a href="{{ route('login') }}">Log in</a>
+                                    </div>
+                                    <a href="{{ route('password.request') }}" style="float:right">Forgot Password?</a>
+                                </p>
+                                <div class="boder3"></div>
+                            </div>
+                            <div class="clearfix"> </div><br>
+                            <p class="mt-5 mb-3 text-muted text-center">© 2017 - {{ date('Y') }}</p>
+                            Email us if you have login problems :<a href="mailto:{{ setting('site.email') }}"> {{ setting('site.email') }} </a>
+                            {{ Form::close() }}
+                            <div class="clearfix"> </div>
+                        </div>
+                    </div>
+                    <!--login-right-side-->
+                    <div class="col-md-4 col-12">
+                        <div class="blog-in">
+                            <div class="mb-4">
+                                <h3 class="h2 mb-3 font-weight-normal">Benefits</h3>
+                            </div>
+                            <div class="clearfix"> </div>
+                            <div class="mb-4">
+                                <h3 class="h5 mb-3 font-weight-normal"><b>Save time for now.</b></h3>
+                                <h3 class="h5 mb-3 font-weight-normal">
+                                    Creating an account will allow you to <br> checkout faster in the feature, have easy <br> access to order history and customize your <br> experience to suit your preference.
+                                </h3>
+                            </div>
+                            <div class="mb-4">
+                                <h3 class="h2 mb-3 font-weight-normal">Terms and Conditions</h3>
+                            </div>
+                            <div class="mb-4">
+                                <h3 class="h5 mb-3 font-weight-normal"><b>By clicking on "Create Acount" you agree to The Company's' Terms and Conditions</b></h3>
+                                <h3 class="h5 mb-3 font-weight-normal">
+                                    While rare, prices are subject to change based on exchange rate fluctuations - should such a fluctuation happen, we may request an additional payment. You have the option to request a full refund or to pay the new price.
+                                </h3>
+                            </div>
+                            <div class="mb-4">
+                                <h3 class="h5 mb-3 font-weight-normal">
+                                    Should there be an error in the description or <br> pricing of a product, we will provide you with a full refund
+                                </h3>
+                            </div>
+                            <div class="mb-4">
+                                <h3 class="h5 mb-3 font-weight-normal">
+                                    Acceptance of an order by us is dependent on our <br> suppliers ability to provide the product.
+                                </h3>
+                            </div>
+                            <div class="clearfix"> </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="clearfix"> </div>
-            @include('shop.messages.error')
-            <div class="clearfix"> </div>
-            {!! Form::open(array('route'=>'register','method' => 'POST','class' => 'form-signup')) !!}
-            {{ csrf_field() }}
-            {!!  GoogleReCaptchaV3::render('register') !!}
-            <div class="text-center mb-4">
-                <h3 class="h3 mb-3 font-weight-normal">{{ __('Sign Un New Profile') }}</h3>
-            </div>
-
-            <div class="form-label-group">
-                {{ Form::text('name', old('name'), array('class' => 'form-control', 'placeholder' => 'Full Name', 'id' => 'inputName', $errors->has('name') ? ' is-invalid' : '','required', 'autofocus')) }}
-                {{ Form::label('inputName', 'Full Name') }}
-            </div>
-
-            <div class="form-label-group">
-                {{ Form::email('email', old('email'), array('class' => 'form-control', 'placeholder' => 'Email address', 'id' => 'inputEmail', $errors->has('email') ? ' is-invalid' : '','required')) }}
-                {{ Form::label('inputEmail', 'E-Mail Address') }}
-            </div>
-
-            <div class="form-label-group">
-                <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" {{ $errors->has('password') ? ' is-invalid' : '' }} required>
-                {{ Form::label('inputPassword', 'Password') }}
-            </div>
-
-            <div class="form-label-group">
-                <input type="password" name="password_confirmation" id="inputPasswordConfirmed" class="form-control" placeholder="Re-type Password" required>
-                {{ Form::label('inputPasswordConfirmed', 'Re-type Password') }}
-            </div>
-
-            {!! Form::button('Create Acount',array('class'=>'btn btn-default btn-lg button-1 btn-block', 'type' => 'submit')) !!}
-            <div class="clearfix"> </div><br>
-            <div class="form-group">
-                <div class="boder3"></div>
-                <p>
-                    <div style="float:left">
-                        Already have an account? <a href="{{ route('login') }}">Log in</a>
-                    </div>
-                    <a href="{{ route('password.request') }}" style="float:right">Forgot Password?</a>
-                </p>
-                <div class="boder3"></div>
-            </div>
-            <div class="clearfix"> </div><br>
-            <p class="mt-5 mb-3 text-muted text-center">© 2017 - {{ date('Y') }}</p>
-            Email us if you have login problems :<a href="mailto:{{ setting('site.email') }}"> {{ setting('site.email') }} </a>
-            {{ Form::close() }}
-            <div class="clearfix"> </div>
+            <div class="clearfix"></div>
         </div>
+        <!--container-->
         <div class="clearfix"></div>
     </div>
-    <!--container-->
-    <div class="clearfix"></div>
 @endsection
