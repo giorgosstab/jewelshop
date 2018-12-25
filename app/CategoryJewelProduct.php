3c80868bc22d1daa0d7e8aad5000877fc2d4021f
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2018-12-25 16:28:22 +0200

    Import custom overwrite voyager product (view-controller)

--- app/CategoryJewelProduct.php
+++ app/CategoryJewelProduct.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class CategoryJewelProduct extends Model
 {
     protected $table = 'category_jewel_product';
+
+    protected $fillable = [
+        'product_id',
+        'category_jewel_id'
+    ];
 }
