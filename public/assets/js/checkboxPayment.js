660bbeb3a380ed5f0c182e9cff229264390f2cf4
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2019-01-18 04:20:15 +0200

    Add code for double click

--- public/assets/js/checkboxPayment.js
+++ public/assets/js/checkboxPayment.js
@@ -1,7 +1,14 @@
 $(document).ready(function () {
+    $(".checkPayment").dblclick(function(e){
+        e.preventDefault();
+        e.prop('checked',false);
+        e.collapse('hide');
+    });
+
     $(".checkPayment").on('change', function() {
         var checked = $(this).is(':checked');
         $(".checkPayment").prop('checked',false);
+        $(".checkPayment").collapse('hide');
         if(checked) {
             $(this).prop('checked',true);
         }
