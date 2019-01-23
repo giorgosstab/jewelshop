d81509c883b8457c10afce6a2d544a5e6ff3e32f
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2019-01-24 01:16:27 +0200

    Fix bug when user is auth cant redirect to checkout-guest

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -31,10 +31,6 @@ class CheckoutController extends Controller
             return redirect()->route('shop.products.index');
         }
 
-        if(auth()->user() && request()->is('checkout-guest')) {
-            return redirect()->route('shop.checkout.index');
-        }
-
         return view('shop.checkout.main')->with([
             'discount' => $this->getNumbers()->get('discount'),
             'newSubTotal' => $this->getNumbers()->get('newSubTotal'),
@@ -57,6 +53,10 @@ class CheckoutController extends Controller
 
         if(Cart::instance('default')->count() == 0) {
             return redirect()->route('shop.products.index');
+        }
+
+        if(auth()->user() && request()->is('checkout-guest')) {
+            return redirect()->route('shop.checkout.index');
         }
 
         return view('shop.checkout.main')->with([
