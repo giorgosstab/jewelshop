487b690828aaec5cfde08461eba1301ca2d4e51e
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2019-01-27 13:46:27 +0200

    Move mail address and name to env file

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'info@jewelshop.io'),
-        'name' => env('MAIL_FROM_NAME', 'Jewelshop'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
     /*
