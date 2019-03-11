e2260fc54d9b2b7921e585d74e727e1e9e282110
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2019-03-12 01:27:08 +0200

    Fix all conditions when change layout

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,25 +7,34 @@ use App\Product;
 
 class PagesController extends Controller
 {
-    public function index()
-    {
-        $page = CustomPage::where('slug', '/')->where('active', 1)->first();
-        return view('page')->with('page', $page);
-    }
-
     public function getPage($slug = null)
     {
-        $specialOffers = Product::where('status', 'like', 'PUBLISHED')->where('offer', true)->inRandomOrder()->take(4)->get();
-        $hotDeals = Product::where('status', 'like', 'PUBLISHED')->where('hotdeals', true)->inRandomOrder()->take(2)->get();
-
         $page = CustomPage::where('slug', $slug)->where('status', 'active');
         $page = $page->firstOrFail();
 
-        // return view($page->template)->with('page', $page);
-        return view('shop.pages.main')->with([
-            'page' => $page,
-            'specialOffers' => $specialOffers,
-            'hotDeals' => $hotDeals,
-        ]);
+        if($page->layout == CustomPage::LAYOUT_CONTAINER_DEFAULT){
+            $specialOffers = Product::where('status', 'like', 'PUBLISHED')->where('offer', true)->inRandomOrder()->take(4)->get();
+            $hotDeals = Product::where('status', 'like', 'PUBLISHED')->where('hotdeals', true)->inRandomOrder()->take(2)->get();
+
+            return view('shop.pages.default')->with([
+                'page' => $page,
+                'specialOffers' => $specialOffers,
+                'hotDeals' => $hotDeals,
+            ]);
+        } else {
+            $specialOffers = Product::where('status', 'like', 'PUBLISHED')->where('offer', true)->inRandomOrder()->take(4)->get();
+            $hotDeals = Product::where('status', 'like', 'PUBLISHED')->where('hotdeals', true)->inRandomOrder()->take(2)->get();
+            if($page->column_right_left == CustomPage::COLUMN_RIGHT ||$page->column_right_left == CustomPage::COLUMN_LEFT ){
+                return view('shop.pages.fluid')->with([
+                    'page' => $page,
+                    'specialOffers' => $specialOffers,
+                    'hotDeals' => $hotDeals,
+                ]);
+            } else {
+                return view('shop.pages.fluid')->with([
+                    'page' => $page,
+                ]);
+            }
+        }
     }
 }
