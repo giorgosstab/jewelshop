2f2121dcf53ede0f04e905f6cd7cd899be8a1b29
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2019-03-16 21:36:33 +0200

    Remove Default dummies of voyager

--- database/seeds/VoyagerDummyDatabaseSeeder.php
+++ database/seeds/VoyagerDummyDatabaseSeeder.php
@@ -17,11 +17,11 @@ class VoyagerDummyDatabaseSeeder extends Seeder
     public function run()
     {
         $this->seedersPath = database_path('seeds').'/';
-        $this->seed('CategoriesTableSeeder');
+//        $this->seed('CategoriesTableSeeder');
         $this->seed('UsersTableSeeder');
-        $this->seed('PostsTableSeeder');
-        $this->seed('PagesTableSeeder');
-        $this->seed('TranslationsTableSeeder');
+//        $this->seed('PostsTableSeeder');
+//        $this->seed('PagesTableSeeder');
+//        $this->seed('TranslationsTableSeeder');
         $this->seed('PermissionRoleTableSeeder');
     }
 }
