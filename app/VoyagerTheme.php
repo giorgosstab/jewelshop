0a3976c605eaec100138a9263e7c0530f4f91a2b
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2019-03-29 22:50:55 +0200

    Uncomment the relationship to have access in helpers

--- app/VoyagerTheme.php
+++ app/VoyagerTheme.php
@@ -9,7 +9,7 @@ class VoyagerTheme extends Model
     protected $table = 'voyager_themes';
     protected $fillable = ['name', 'folder', 'version'];
 
-//    public function options(){
-//        return $this->hasMany('App\VoyagerThemeOption', 'voyager_theme_id');
-//    }
+    public function options(){
+        return $this->hasMany('App\VoyagerThemeOption', 'voyager_theme_id');
+    }
 }
