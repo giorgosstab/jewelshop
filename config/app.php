dc5f0fbd03d4e1f565c8e47e6cb1a085ecf77150
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2019-03-29 22:24:03 +0200

    Import my custom provider to app config

--- config/app.php
+++ config/app.php
@@ -166,6 +166,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        App\Providers\VoyagerThemesServiceProvider::class,
+
     ],
 
     /*
