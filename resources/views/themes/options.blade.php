049ec2e3630bf25e56dca36277eef5b7ab2c8fe7
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2019-03-31 07:33:29 +0300

    Fix css in a file and add csrf token

--- resources/views/themes/options.blade.php
+++ resources/views/themes/options.blade.php
@@ -1,43 +1,8 @@
 @extends('voyager::master')
 
 @section('css')
-
-	<style type="text/css">
-
-		#theme_options .page-title small{
-			margin-left:10px;
-			color:rgba(0, 0, 0, 0.55);
-		}
-
-		#theme_options label, :not(#theme_options .toggle-group label){
-			font-weight: normal;
-		    font-size: 16px;
-		    width: 100%;
-		    margin-bottom: 10px;
-		    position: relative;
-		    left: 1px;
-		}
-
-		#theme_options span.how_to {
-		    font-size: 12px;
-		    margin-left: 10px;
-		    background: #fff;
-		    padding: 5px 14px;
-		    border-radius: 15px;
-		    display: inline-block;
-		    margin-top: 10px;
-		    border: 1px solid #f1f1f1;
-		    position: relative;
-		    top: -1px;
-		}
-
-		#theme_options span.how_to code{
-			background: none;
-    		border: 0px;
-		}
-
-	</style>
-
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    {{ Html::style('assets/css/voyager.theme.css') }}
 @endsection
 
 @section('content')
