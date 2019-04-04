273c1cf80403a167ea8a53ab3fb14b1081f2b8ce
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2019-04-04 23:16:48 +0300

    Change seeder stars from super admin to a random user

--- database/seeds/RatingTableSeeder.php
+++ database/seeds/RatingTableSeeder.php
@@ -2,6 +2,7 @@
 
 use App\Product;
 use App\Rating;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class RatingTableSeeder extends Seeder
@@ -17,7 +18,7 @@ class RatingTableSeeder extends Seeder
         for($i = 1; $i <= $products->count(); $i++){
             for($j = 0; $j < 5; $j++) {
                 Rating::create([
-                    'user_id' => 1,
+                    'user_id' => User::all()->random()->id,
                     'product_id' => $i,
                     'rating' => rand(1,5),
                 ]);
