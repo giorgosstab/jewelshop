261a77c59c029a7e0a3f06eec56a6ebe872dcf89
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2019-04-06 19:14:15 +0300

    Import override user voyager view edit-add and controller to store user Details

---
+++ app/Http/Controllers/Voyager/UsersController.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace App\Http\Controllers\Voyager;
+
+use App\UserDetail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use TCG\Voyager\Events\BreadDataAdded;
+use TCG\Voyager\Events\BreadDataUpdated;
+use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Http\Controllers\Traits\BreadRelationshipParser;
+use TCG\Voyager\Http\Controllers\VoyagerBaseController;
+use App\User;
+
+class UsersController extends VoyagerBaseController
+{
+    use BreadRelationshipParser;
+
+    //***************************************
+    //                ______
+    //               |  ____|
+    //               | |__
+    //               |  __|
+    //               | |____
+    //               |______|
+    //
+    //  Edit an item of our Data Type BR(E)AD
+    //
+    //****************************************
+
+    public function edit(Request $request, $id)
+    {
+        $slug = $this->getSlug($request);
+
+        $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
+
+        $relationships = $this->getRelationships($dataType);
+
+        $dataTypeContent = (strlen($dataType->model_name) != 0)
+            ? app($dataType->model_name)->with($relationships)->findOrFail($id)
+            : DB::table($dataType->name)->where('id', $id)->first(); // If Model doest exist, get data from table name
+
+        $user = User::with('userDetail')->findOrFail($id);
+
+        foreach ($dataType->editRows as $key => $row) {
+            $dataType->editRows[$key]['col_width'] = isset($row->details->width) ? $row->details->width : 100;
+        }
+
+        // If a column has a relationship associated with it, we do not want to show that field
+        $this->removeRelationshipField($dataType, 'edit');
+
+        // Check permission
+        $this->authorize('edit', $dataTypeContent);
+
+        // Check if BREAD is Translatable
+        $isModelTranslatable = is_bread_translatable($dataTypeContent);
+
+        $view = 'voyager::bread.edit-add';
+
+        if (view()->exists("voyager::$slug.edit-add")) {
+            $view = "voyager::$slug.edit-add";
+        }
+
+        return Voyager::view($view, compact('dataType', 'dataTypeContent', 'isModelTranslatable', 'user'));
+    }
+
+    // POST BR(E)AD
+    public function update(Request $request, $id)
+    {
+        $slug = $this->getSlug($request);
+
+        $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
+
+        // Compatibility with Model binding.
+        $id = $id instanceof Model ? $id->{$id->getKeyName()} : $id;
+
+        $data = call_user_func([$dataType->model_name, 'findOrFail'], $id);
+
+        // Check permission
+        $this->authorize('edit', $data);
+
+        // Validate fields with ajax
+        $val = $this->validateBread($request->all(), $dataType->editRows, $dataType->name, $id);
+
+        if ($val->fails()) {
+            return response()->json(['errors' => $val->messages()]);
+        }
+
+        if (!$request->ajax()) {
+
+            $user = User::find($id);
+            $details = $this->updateDetailsUser($request);
+
+            if($user->userDetail()->first()) {
+                // add my override request and save to details table
+                $user->userDetail()->update([
+                    'phone' => $details->phone,
+                    'company' => $details->company,
+                    'city' => $details->city,
+                    'country' => $details->country,
+                    'address' => $details->address,
+                    'house_number' => $details->house_number,
+                    'postal_code' => $details->postal_code,
+                    'locality' => $details->locality,
+                ]);
+            } else {
+                $user->userDetail()->save($details);
+            }
+
+            $this->insertUpdateData($request, $slug, $dataType->editRows, $data);
+
+            event(new BreadDataUpdated($dataType, $data));
+
+            return redirect()
+                ->route("voyager.{$dataType->slug}.index")
+                ->with([
+                    'message'    => __('voyager::generic.successfully_updated')." {$dataType->display_name_singular}",
+                    'alert-type' => 'success',
+                ]);
+        }
+    }
+
+    /**
+     * POST BRE(A)D - Store data.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $slug = $this->getSlug($request);
+
+        $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
+
+        // Check permission
+        $this->authorize('add', app($dataType->model_name));
+
+        // Validate fields with ajax
+        $val = $this->validateBread($request->all(), $dataType->addRows);
+
+        if ($val->fails()) {
+            return response()->json(['errors' => $val->messages()]);
+        }
+
+        if (!$request->has('_validate')) {
+            $data = $this->insertUpdateData($request, $slug, $dataType->addRows, new $dataType->model_name());
+
+            //add details for inserted id user
+            $user = User::find($data->id);
+            $details = $this->updateDetailsUser($request);
+            $user->userDetail()->save($details);
+
+            event(new BreadDataAdded($dataType, $data));
+
+            if ($request->ajax()) {
+                return response()->json(['success' => true, 'data' => $data]);
+            }
+
+            return redirect()
+                ->route("voyager.{$dataType->slug}.index")
+                ->with([
+                        'message'    => __('voyager::generic.successfully_added_new')." {$dataType->display_name_singular}",
+                        'alert-type' => 'success',
+                    ]);
+        }
+    }
+
+    protected function updateDetailsUser($request){
+        $details = new userDetail;
+        $details->phone = $request->phone;
+        $details->company = $request->company;
+        $details->city = $request->city;
+        $details->country = $request->country;
+        $details->address = $request->address;
+        $details->house_number = $request->house_number;
+        $details->postal_code = $request->postal_code;
+        $details->locality = $request->locality;
+        return $details;
+    }
+}
---
+++ resources/views/vendor/voyager/users/edit-add.blade.php
@@ -0,0 +1,188 @@
+@extends('voyager::master')
+
+@section('page_title', __('voyager::generic.'.(isset($dataTypeContent->id) ? 'edit' : 'add')).' '.$dataType->display_name_singular)
+
+@section('css')
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+@stop
+
+@section('page_header')
+    <h1 class="page-title">
+        <i class="{{ $dataType->icon }}"></i>
+        {{ __('voyager::generic.'.(isset($dataTypeContent->id) ? 'edit' : 'add')).' '.$dataType->display_name_singular }}
+    </h1>
+@stop
+
+@section('content')
+    <div class="page-content container-fluid">
+        <form class="form-edit-add" role="form"
+              action="{{ (isset($dataTypeContent->id)) ? route('voyager.'.$dataType->slug.'.update', $dataTypeContent->id) : route('voyager.'.$dataType->slug.'.store') }}"
+              method="POST" enctype="multipart/form-data" autocomplete="off">
+            <!-- PUT Method if we are editing -->
+            @if(isset($dataTypeContent->id))
+                {{ method_field("PUT") }}
+            @endif
+            {{ csrf_field() }}
+
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="panel panel-bordered">
+                    {{-- <div class="panel"> --}}
+                        @if (count($errors) > 0)
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+
+                        <div class="panel-body">
+                            <div class="form-group">
+                                <label for="name">{{ __('voyager::generic.name') }}</label>
+                                <input type="text" class="form-control" id="name" name="name" placeholder="{{ __('voyager::generic.name') }}"
+                                       value="@if(isset($dataTypeContent->name)){{ $dataTypeContent->name }}@endif">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="email">{{ __('voyager::generic.email') }}</label>
+                                <input type="email" class="form-control" id="email" name="email" placeholder="{{ __('voyager::generic.email') }}"
+                                       value="@if(isset($dataTypeContent->email)){{ $dataTypeContent->email }}@endif">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="password">{{ __('voyager::generic.password') }}</label>
+                                @if(isset($dataTypeContent->password))
+                                    <br>
+                                    <small>{{ __('voyager::profile.password_hint') }}</small>
+                                @endif
+                                <input type="password" class="form-control" id="password" name="password" value="" autocomplete="new-password">
+                            </div>
+
+                            @can('editRoles', $dataTypeContent)
+                                <div class="form-group">
+                                    <label for="default_role">{{ __('voyager::profile.role_default') }}</label>
+                                    @php
+                                        $dataTypeRows = $dataType->{(isset($dataTypeContent->id) ? 'editRows' : 'addRows' )};
+
+                                        $row     = $dataTypeRows->where('field', 'user_belongsto_role_relationship')->first();
+                                        $options = $row->details;
+                                    @endphp
+                                    @include('voyager::formfields.relationship')
+                                </div>
+                                <div class="form-group">
+                                    <label for="additional_roles">{{ __('voyager::profile.roles_additional') }}</label>
+                                    @php
+                                        $row     = $dataTypeRows->where('field', 'user_belongstomany_role_relationship')->first();
+                                        $options = $row->details;
+                                    @endphp
+                                    @include('voyager::formfields.relationship')
+                                </div>
+                            @endcan
+                            @php
+                            if (isset($dataTypeContent->locale)) {
+                                $selected_locale = $dataTypeContent->locale;
+                            } else {
+                                $selected_locale = config('app.locale', 'en');
+                            }
+
+                            @endphp
+                            <div class="form-group">
+                                <label for="locale">{{ __('voyager::generic.locale') }}</label>
+                                <select class="form-control select2" id="locale" name="locale">
+                                    @foreach (Voyager::getLocales() as $locale)
+                                    <option value="{{ $locale }}"
+                                    {{ ($locale == $selected_locale ? 'selected' : '') }}>{{ $locale }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="phone">Phone</label>
+                                <input type="text" class="form-control" id="phone" name="phone" placeholder="Phone"
+                                       value="@if(isset($user->userDetail->phone)){{ $user->userDetail->phone }}@endif">
+                            </div>
+                            <div class="form-group">
+                                <label for="company">Company</label>
+                                <input type="text" class="form-control" id="company" name="company" placeholder="Company"
+                                       value="@if(isset($user->userDetail->company)){{ $user->userDetail->company }}@endif">
+                            </div>
+                            <div class="form-group">
+                                <label for="city">City</label>
+                                <input type="text" class="form-control" id="city" name="city" placeholder="City"
+                                       value="@if(isset($user->userDetail->city)){{ $user->userDetail->city }}@endif">
+                            </div>
+                            <div class="form-group">
+                                <label for="country">Country</label>
+                                <select class="form-control" name="country" id="country">
+                                    <option value="" disabled="" selected="selected">Please select your country</option>
+                                    <option @if(!empty($user->userDetail->country))@if($user->userDetail->country == 'GREECE'){{ 'selected' }}@else{{ old('country') == 'GREECE' ? 'selected' : '' }}@endif @endif value="GREECE">GREECE </option>
+                                    <option @if(!empty($user->userDetail->country))@if($user->userDetail->country == 'CYPRUS'){{ 'selected' }}@else{{ old('country') == 'CYPRUS' ? 'selected' : '' }}@endif @endif value="CYPRUS">CYPRUS </option>
+                                </select>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="address">Address</label>
+                                        <input type="text" class="form-control" id="address" name="address" placeholder="Address"
+                                               value="@if(isset($user->userDetail->address)){{ $user->userDetail->address }}@endif">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="house_number">House Number</label>
+                                        <input type="text" class="form-control" id="house_number" name="house_number" placeholder="House Number"
+                                               value="@if(isset($user->userDetail->house_number)){{ $user->userDetail->house_number }}@endif">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="postal_code">Postal Code</label>
+                                <input type="text" class="form-control" id="postal_code" name="postal_code" placeholder="Postal Code"
+                                       value="@if(isset($user->userDetail->postal_code)){{ $user->userDetail->postal_code }}@endif">
+                            </div>
+                            <div class="form-group">
+                                <label for="locality">Locality</label>
+                                <input type="text" class="form-control" id="locality" name="locality" placeholder="Locality"
+                                       value="@if(isset($user->userDetail->locality)){{ $user->userDetail->locality }}@endif">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="panel panel panel-bordered panel-warning">
+                        <div class="panel-body">
+                            <div class="form-group">
+                                @if(isset($dataTypeContent->avatar))
+                                    <img src="{{ filter_var($dataTypeContent->avatar, FILTER_VALIDATE_URL) ? $dataTypeContent->avatar : Voyager::image( $dataTypeContent->avatar ) }}" style="width:200px; height:auto; clear:both; display:block; padding:2px; border:1px solid #ddd; margin-bottom:10px;" />
+                                @endif
+                                <input type="file" data-name="avatar" name="avatar">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <button type="submit" class="btn btn-primary pull-right save">
+                {{ __('voyager::generic.save') }}
+            </button>
+        </form>
+
+        <iframe id="form_target" name="form_target" style="display:none"></iframe>
+        <form id="my_form" action="{{ route('voyager.upload') }}" target="form_target" method="post" enctype="multipart/form-data" style="width:0px;height:0;overflow:hidden">
+            {{ csrf_field() }}
+            <input name="image" id="upload_file" type="file" onchange="$('#my_form').submit();this.value='';">
+            <input type="hidden" name="type_slug" id="type_slug" value="{{ $dataType->slug }}">
+        </form>
+    </div>
+@stop
+
+@section('javascript')
+    <script>
+        $('document').ready(function () {
+            $('.toggleswitch').bootstrapToggle();
+        });
+    </script>
+@stop
