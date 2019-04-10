a5ac75fb0896ecb804936cf703a72014110a9ced
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2019-04-10 04:44:39 +0300

    remove vendor file from git

--- vendor/timehunter/laravel-google-recaptcha-v3/resources/views/googlerecaptchav3/field.blade.php
+++
@@ -1,9 +0,0 @@
-<input type="hidden" id="{{$id}}" name="{{$name}}">
-<script>
-    grecaptcha.ready(function () {
-        grecaptcha.execute('{{$publicKey}}', {action: '{{$action}}'}).then(function (token) {
-            console.log(token);
-            document.getElementById('{{$id}}').value = token;
-        });
-    });
-</script>
