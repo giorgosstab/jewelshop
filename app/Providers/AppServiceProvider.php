5482c7c28c1f5e6801d656573e8a57d8e9f04391
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2019-04-20 15:44:26 +0300

    Add redirect for production in https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //for mariadb (xampp server) and not MySQL (laragon server)
+//        Schema::defaultStringLength(191);
+
+        //for redirect routes only up to https protocol
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 
     /**
