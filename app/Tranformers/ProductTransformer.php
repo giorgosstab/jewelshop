e31c6d64692cf419e27b4b490e758844fe7ca7d7
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2019-05-12 01:33:09 +0300

    Import columns on Product Details API

--- app/Tranformers/ProductTransformer.php
+++ app/Tranformers/ProductTransformer.php
@@ -10,16 +10,26 @@ class ProductTransformer extends TransformerAbstract
 
     public function transform(Product $product)
     {
+        $images = collect([]);
+        if($product->images && $product->images!=="[]"){
+            foreach(json_decode($product->images, true) as $img){
+                $images->push(url('storage/'.$img));
+            }
+        }
         return [
             'id'          => (int) $product->id,
             'name'        => $product->name,
             'slug'        => $product->slug,
             'sku'         => $product->sku,
+            'price'       => $product->price,
+            'secondprice'       => $product->secondprice,
+            'quantity'       => $product->quantity,
+            'hotdeals'       => $product->hotdeals,
             'brand'       => [
                 'name' => $product->brand->name,
             ],
             'image'       => productImage($product->image),
-            'images'      => productImage($product->images),
+            'images'      => $images,
             'rating'      => round($product->ratings->avg('rating')),
             'description' => $product->description,
             'created_at'  => $product->created_at->format('d M Y - H:i:s'),
