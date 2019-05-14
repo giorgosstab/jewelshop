1a7391c728fc36ac7e6d0deb6191ebd72d6fcd67
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2019-05-14 20:45:58 +0300

    Add feature delete item from wish list

--- app/Http/Controllers/API/v1/WishlistController.php
+++ app/Http/Controllers/API/v1/WishlistController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API\v1;
 
 use App\Http\Controllers\Controller;
 use App\Transformer\UserWishlistTransformer;
+use App\User;
 use App\Wishlist;
 use League\Fractal\Manager;
 use League\Fractal\Pagination\IlluminatePaginatorAdapter;
@@ -46,5 +47,27 @@ class WishlistController extends Controller
         $wishlists = $this->fractal->createData($resource); // Transform data
 
         return response()->json($wishlists->toArray());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $user_id
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($user_id,$id)
+    {
+        $user = User::find($user_id);
+        $wishlist = Wishlist::where('user_id',$user->id)->find($id);
+        if($wishlist){
+            $wishlist->delete();
+            if(!$wishlist){
+                return response()->json(['message' => 'Something Went Wrong','status_code' => 404]);
+            }
+            return response()->json(['message' => 'Successfully Delete Items','status_code' => 200]);
+        } else {
+            return response()->json(['message' => 'You must Delete only your wish list items','status_code' => 404]);
+        }
     }
 }
