e4d3d70a0acfd05dd2312b648a033a8c0dd6a566
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2019-05-20 19:07:59 +0300

    Remove integers from API details user

--- app/Tranformers/UserDetailTransformer.php
+++ app/Tranformers/UserDetailTransformer.php
@@ -11,13 +11,13 @@ class UserDetailTransformer extends TransformerAbstract
     public function transform(UserDetail $user)
     {
         return [
-            'phone'        => (int) $user->phone,
+            'phone'        => $user->phone,
             'company'      => $user->company,
             'city'         => $user->city,
             'country'      => $user->country,
             'address'      => $user->address,
-            'house_number' => (int) $user->house_number,
-            'zip'          => (int) $user->postal_code,
+            'house_number' => $user->house_number,
+            'zip'          => $user->postal_code,
             'locality'     => $user->locality,
         ];
     }
