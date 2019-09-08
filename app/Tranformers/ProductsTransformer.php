6abc9ad1153c70e18a3a1beb01d55d390aad2844
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2019-09-08 17:24:15 +0300

    Change API for all products to get all details of each product

--- app/Tranformers/ProductsTransformer.php
+++ app/Tranformers/ProductsTransformer.php
@@ -10,13 +10,35 @@ class ProductsTransformer extends TransformerAbstract
 
     public function transform(Product $product)
     {
+        $images = collect([]);
+        if($product->images && $product->images!=="[]"){
+            foreach(json_decode($product->images, true) as $img){
+                $images->push(url('storage/'.$img));
+            }
+        }
+
+        $discount = $product->price - $product->secondprice;
+        $percent = $product->presentPriceDeals() != null ? ($discount / $product->price) * 100 : 0;
+
         return [
-            'id'      => (int) $product->id,
-            'name'   => $product->name,
-            'slug'    => $product->slug,
-            'image' => productImage($product->image),
-            'rating' => round($product->ratings->avg('rating')),
-            'created_at' => $product->created_at->format('d M Y - H:i:s'),
+            'id'          => (int) $product->id,
+            'name'        => $product->name,
+            'slug'        => $product->slug,
+            'sku'         => $product->sku,
+            'price'       => $product->presentPrice(),
+            'price_int'       => $product->price,
+            'secondprice' => $product->presentPriceDeals(),
+            'discount'    => number_format((float)$percent, 2, '.', ''),
+            'quantity'    => $product->quantity,
+            'hotdeals'    => $product->hotdeals,
+            'brand'       => [
+                'name' => $product->brand->name,
+            ],
+            'image'       => productImage($product->image),
+            'images'      => $images,
+            'rating'      => round($product->ratings->avg('rating')),
+            'description' => $product->description,
+            'created_at'  => $product->created_at->format('d M Y - H:i:s'),
         ];
     }
 }
